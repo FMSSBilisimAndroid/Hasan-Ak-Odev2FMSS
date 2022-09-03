49d2d813feeebd4b12be2d41ac931fa860f45755
developerhasanak <hasan_ak_44@outlook.com>
2022-09-03 12:11:39 +0300

    get started screen update

--- app/src/main/java/com/dag/odev2fmss/GetStartedActivity.kt
+++ app/src/main/java/com/dag/odev2fmss/GetStartedActivity.kt
@@ -1,11 +1,18 @@
 package com.dag.odev2fmss
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.dag.odev2fmss.databinding.ActivityGetstartedBinding
 
 class GetStartedActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityGetstartedBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_getstarted)
+        binding = ActivityGetstartedBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
 }

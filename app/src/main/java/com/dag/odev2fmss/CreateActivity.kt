331aaf4cd43f7fa441c857dc9488a337df0d2b90
developerhasanak <hasan_ak_44@outlook.com>
2022-09-04 13:22:06 +0300

    create screen complate

--- app/src/main/java/com/dag/odev2fmss/CreateActivity.kt
+++ app/src/main/java/com/dag/odev2fmss/CreateActivity.kt
@@ -1,11 +1,18 @@
 package com.dag.odev2fmss
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
 class CreateActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create)
+    }
+
+    fun backButton2(view: View) {
+       val intent = Intent(this,LoginActivity::class.java)
+       startActivity(intent)
     }
 }

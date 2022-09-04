1729f6bd4b617a408369a761292c2b406a4a93e0
developerhasanak <hasan_ak_44@outlook.com>
2022-09-04 23:39:20 +0300

    application completed

--- app/src/main/java/com/dag/odev2fmss/GetStartedActivity.kt
+++ app/src/main/java/com/dag/odev2fmss/GetStartedActivity.kt
@@ -1,6 +1,9 @@
 package com.dag.odev2fmss
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
@@ -9,15 +12,29 @@ import com.dag.odev2fmss.databinding.ActivityGetstartedBinding
 class GetStartedActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityGetstartedBinding
+    private lateinit var sharedPreferences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityGetstartedBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        createSharedPreferences()
     }
 
     fun joinNowClick(view: View) {
         val intent = Intent(this@GetStartedActivity, LoginActivity::class.java)
         startActivity(intent)
     }
+
+
+    private fun createSharedPreferences(){
+        sharedPreferences = this.getSharedPreferences("SINGUP", Context.MODE_PRIVATE)
+        sharedPreferences.edit().putString("EMAIL","hasan@gmail").apply()
+        sharedPreferences.edit().putString("USERNAME","hasan").apply()
+        sharedPreferences.edit().putString("PASSWORD","123456").apply()
+
+
+    }
+
 }
---
+++ app/src/main/java/com/dag/odev2fmss/UserData.kt
@@ -0,0 +1,8 @@
+package com.dag.odev2fmss
+
+data class UserData(
+    val email: String,
+    val userName: String,
+    val password: String
+)
+
